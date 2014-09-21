4e6480887983e42e79600adc537a150347c944fd
Luiz Romário Filho <luizromariofilho@gmail.com>
2014-09-21 17:11:58 -0300

    Add Header and Footer
    Adicionei cabeçalho e rodapé no projeto

--- cabecalho.php
+++ cabecalho.php
@@ -3,7 +3,11 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
 		<title>Minha Loja - <?= $title ?></title>
 	</head>
 	<body>
 		<div class="container">
+			<div class="page-header">
+	        	<h1>Bem Vindo à Loja</h1>
+	      	</div>
--- rodape.php
+++ rodape.php
@@ -1,4 +1,10 @@
-		
-		</div>
+		</div> <!-- end container -->
+		<div class="footer">
+  			<div class="container">
+	        	<p class="text-muted">
+					<a href="https://github.com/tatacbl" target="_blank">&copy Talita Carla</a>
+	        	</p>
+	      	</div>
+	    </div><!-- end footer -->
 	</body>
 </html>
